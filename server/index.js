4d7e99821905077fe741fe79e5594bb488a25244
Dane Maison <danemaison@gmail.com>
2019-11-14 23:53:14 -0800

    added update application route

--- server/index.js
+++ server/index.js
@@ -32,6 +32,12 @@ app.post('/api/add-application', (req, res) => {
     res.send(result);
   });
 });
+
+app.put('/api/update-application', (req, res) => {
+  console.log(req.body);
+  // const sql = `UPDATE`
+});
+
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Listening on port ${process.env.PORT}`);
